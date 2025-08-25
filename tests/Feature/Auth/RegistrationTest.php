7e50db7e0b3098625a304c3131fbb1c6df7cd5ac
ndrskln <github.com@ndrskln.de>
2025-08-25 06:01:40 +0200

    test(user): update tests to expect `name_first` and `name_last`

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -19,7 +19,8 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'name_first' => 'Test',
+            'name_last' => 'User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
